c9b71558124d896ee886a4244e72a0dc0666053c
JuliaMlynarczyk <149715893+JuliaMlynarczyk@users.noreply.github.com>
2025-06-06 13:43:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -601,5 +601,5 @@ def admin_get_patient_appointments():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5001, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
